ccd45a27445ac0b7bf22a7507335f1993ab58869
Frank Lai <franklai2002@gmail.com>
2018-06-05 14:34:30 -0400

    Synced 2

---
+++ Jeopardy_Sync_B2/QuestionDialog.java
@@ -0,0 +1,33 @@
+
+/**
+ * Takes in a question and presents a question dialog. Returns whether or not user got correct answer.
+ *
+ * @author Frank Lai
+ * @version 2018-06-04
+ */
+public class QuestionDialog
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class QuestionDialog
+     */
+    public QuestionDialog()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
